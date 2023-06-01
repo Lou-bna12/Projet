8fdbf11d9a0ac9ceb6127fa72fa8b971907e151a
Loubna Sellam <loubnasellam49@gmail.com>
2023-06-01 16:42:37 +0200

    git commit

---
+++ connexion.php
@@ -0,0 +1,8 @@
+<?php
+$_SERVER="localhost";
+$_USER="root";
+$_PASSWORD="";
+$_DATABASE="garage auto";
+$_PORT="3306";
+$connect =  mysqli_connect($_SERVER,$_USER,$_PASSWORD,$_DATABASE,$_PORT);
+?>
